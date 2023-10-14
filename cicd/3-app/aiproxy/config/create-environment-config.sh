e6f7626058becfa281466c0d2d993fe121fdb55a
Darin Webb <darin@code.org>
2023-10-13 21:40:17 -0500

    add ECS task

--- cicd/3-app/aiproxy/config/create-environment-config.sh
+++ cicd/3-app/aiproxy/config/create-environment-config.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 dir=cicd/3-app/aiproxy
+
+# AMAGE_URI should be set
+if [[ -z "${IMAGE_URI}" ]]; then
+  echo "IMAGE_URI is not set. Please set it to the desired value."
+  exit 1
+fi
 
 # loop over config files
 for i in $(ls "${dir}/config" | egrep -i '.*\.config\.json' ); do
@@ -11,7 +17,8 @@ for i in $(ls "${dir}/config" | egrep -i '.*\.config\.json' ); do
   contents=$(cat $file)
 
   # New value insertion
-  # contents="$(jq '.NewValueDemo = "abcde"' <<< $contents)"
+  contents="$(jq --arg uri "$IMAGE_URI" '.Parameters.AppImageUri = $uri' <<< "$contents")"
+
 
   # Edit existing value
   # if [ "$( jq 'has("TransformDemo")' $file )" == "true" ]; then
