bf51b23e8ad5e303c86893911b22823c3dd44c54
Darin Webb <darin@code.org>
2023-10-18 21:17:08 -0500

    fix typo

--- ci-lint.sh
+++ ci-lint.sh
@@ -3,7 +3,6 @@
 set -e
 
 echo "Validating Cloudformation Templates via cfn-lint docker..."
-docker run --rm -i 
 cfn-lint cicd/1-setup/*.template.yml
 cfn-lint cicd/2-cicd/*.template.yml
 cfn-lint cicd/3-app/aiproxy/template.yml
