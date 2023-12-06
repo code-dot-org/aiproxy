7069d1efb73ee717b5fbdab3684888300e5a2d91
Ceara Chewning <ceara.chewning@code.org>
2023-12-06 18:36:15 -0500

    Add supported lessons and paths to download rubric files

--- lib/assessment/config.py
+++ lib/assessment/config.py
@@ -1,2 +1,11 @@
 VALID_GRADES = ["Extensive Evidence", "Convincing Evidence", "Limited Evidence", "No Evidence"]
 SUPPORTED_MODELS = ['gpt-4', 'gpt-4-0314', 'gpt-4-32k', 'gpt-4-32k-0314']
+LESSONS = {
+    "U3-2022-L10" : "1ROCbvHb3yWGVoQqzKAjwdaF0dSRPUjy_",
+    "U3-2022-L13" : "1kGHeY5LRpFJ9xVRoBEWbyOJyKm4wClqw",
+    "U3-2022-L17" : "1WirJLIFgo-anxAz-kZXDVQ2Tl_8OuX22",
+    "U3-2022-L20" : "115BHvZ1kJC2xhUSOBkLiE8DC1YgcjyRd",
+    "U3-2022-L23" : "12OJex4l9OhWrnbLenpvZAibtfiFWWdzx",
+    "New-U3-2022-L10" : "15xAUFVeGkXeG18mDWBOKN6yJPpI185tg",
+    "New-U3-2022-L13" : "14LI9eRRgxL5rRQK6FoUI0ow_YIb5V0mg",
+}
