3e123f975694a6202ef8b2f9fa327d2450f1203d
Darin Webb <darin@code.org>
2024-01-12 12:55:21 -0600

    silence useless output

--- cicd/2-cicd/deploy-cicd.sh
+++ cicd/2-cicd/deploy-cicd.sh
@@ -39,7 +39,7 @@ TEMPLATE_FILE=cicd/2-cicd/cicd.template.yml
 echo Validating cloudformation template...
 aws cloudformation validate-template \
   --template-body file://${TEMPLATE_FILE} \
-  | cat
+  > /dev/null
 
 ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text)
 
