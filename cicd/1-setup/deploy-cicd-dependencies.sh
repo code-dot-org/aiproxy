8dcffc0a6abcf35da1f90b50f20b151e3c113c74
Darin Webb <darin@code.org>
2024-01-12 00:18:09 -0600

    create reviewable change set instead of blind deploy

--- cicd/1-setup/deploy-cicd-dependencies.sh
+++ cicd/1-setup/deploy-cicd-dependencies.sh
@@ -11,23 +11,38 @@ TEMPLATE_FILE=cicd/1-setup/cicd-dependencies.template.yml
 echo Validating cloudformation template...
 aws cloudformation validate-template \
   --template-body file://${TEMPLATE_FILE} \
-  | cat
+  > /dev/null
 
 ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text)
+REGION=$(aws configure get region)
 
-read -r -p "Would you like to deploy this template to AWS account $ACCOUNT? [y/N] " response
+read -r -p "Would you like to create a change set for this template in AWS account $ACCOUNT? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-  echo Updating cloudformation stack...
-  echo "Follow along at https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks?filteringText=aiproxy-cicd-deps&filteringStatus=active&viewNested=true&hideStacks=false"
-  aws cloudformation deploy \
+  echo Creating change set...
+  CHANGE_SET_NAME="aiproxy-cicd-deps-changeset-$(date +%s)"
+  aws cloudformation create-change-set \
     --stack-name aiproxy-cicd-deps \
-    --template-file ${TEMPLATE_FILE} \
+    --change-set-name $CHANGE_SET_NAME \
+    --template-body file://${TEMPLATE_FILE} \
     --capabilities CAPABILITY_IAM \
     --tags EnvType=infrastructure \
     "$@"
 
-  echo Complete!
+  echo "Change set created. You can review it at:"
+  echo "https://console.aws.amazon.com/cloudformation/home?region=$REGION#/stacks/changesets/changes?stackId=arn:aws:cloudformation:$REGION:$ACCOUNT:stack/aiproxy-cicd-deps/*&changeSetId=arn:aws:cloudformation:$REGION:$ACCOUNT:changeSet/$CHANGE_SET_NAME"
+
+  read -r -p "Would you like to execute the change set? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+  then
+    echo Executing change set...
+    aws cloudformation execute-change-set \
+      --stack-name aiproxy-cicd-deps \
+      --change-set-name $CHANGE_SET_NAME
+    echo Complete!
+  else
+    echo Exiting...
+  fi
 else
   echo Exiting...
 fi
