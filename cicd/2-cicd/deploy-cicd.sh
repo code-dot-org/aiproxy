60d1885add06f3b3f60712c254c05721905c72ce
Darin Webb <darin@code.org>
2024-01-12 00:15:33 -0600

    refactor branch validation and variable defaults

--- cicd/2-cicd/deploy-cicd.sh
+++ cicd/2-cicd/deploy-cicd.sh
@@ -8,38 +8,39 @@ echo Deploying AiProxy CICD Pipeline
 # - ENVIRONMENT_TYPE: Can be 'production' (default) or 'development', passed as a Parameter for "cicd/2-cicd/cicd.template.yml"
 # - GITHUB_BADGE_ENABLED: defaults to true, passed as a Parameter for "cicd/2-cicd/cicd.template.yml"
 
+# The branch name may become part of a domain name, so we need to validate it.
+validate_branch_name() {
+  local branch_name=$1
+  if [[ ! $branch_name =~ ^[a-z0-9]([-a-z0-9]*[a-z0-9])$ ]]; then
+    echo "Invalid branch name '${branch_name}', branches must be alphanumeric and may contain hyphens."
+    exit 1
+  fi
+}
+
 # 'Developer' role requires a specific service role for all CloudFormation operations.
 if [[ $(aws sts get-caller-identity --query Arn --output text) =~ "assumed-role/Developer/" ]]; then
   # Append the role-arn option to the positional parameters $@ passed to cloudformation deploy.
   set -- "$@" --role-arn "arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/admin/CloudFormationService"
 fi
 
-# Default to main branch, but support pipelines using other branches
-TARGET_BRANCH=${TARGET_BRANCH-'main'}
+ENVIRONMENT_TYPE=${ENVIRONMENT_TYPE:-'production'}
+GITHUB_BADGE_ENABLED=${GITHUB_BADGE_ENABLED:-'true'}
+TARGET_BRANCH=${TARGET_BRANCH:-'main'}
 
 if [ "$TARGET_BRANCH" == "main" ]
 then
   STACK_NAME="aiproxy-cicd"
 else
-  # only allow alphanumeric branch names that may contain an internal hyphen.
-  # to avoid complicated logic elsewhere, we're constraining it here.
-  if [[ "$TARGET_BRANCH" =~ ^[a-z0-9]([-a-z0-9]*[a-z0-9])$ ]]; then
-    STACK_NAME="aiproxy-${TARGET_BRANCH}-cicd"
-  else
-    echo "Invalid branch name '${TARGET_BRANCH}', branches must be alphanumeric and may contain hyphens."
-    exit
-  fi
+  validate_branch_name "$TARGET_BRANCH"
+  STACK_NAME="aiproxy-${TARGET_BRANCH}-cicd"
 fi
-
-ENVIRONMENT_TYPE=${ENVIRONMENT_TYPE-'production'}
-GITHUB_BADGE_ENABLED=${GITHUB_BADGE_ENABLED-'true'}
 
 TEMPLATE_FILE=cicd/2-cicd/cicd.template.yml
 
 echo Validating cloudformation template...
 aws cloudformation validate-template \
   --template-body file://${TEMPLATE_FILE} \
-  | cat
+  > /dev/null
 
 ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text)
 
