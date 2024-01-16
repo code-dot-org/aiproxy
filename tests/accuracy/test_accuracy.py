ddc8fe132d3216d874b32a95ce3c845fb57b9354
Ceara Chewning <ceara.chewning@code.org>
2024-01-16 12:01:07 -0500

    Add accuracy unit test, only runs with --accuracy flag to pytest

---
+++ tests/accuracy/test_accuracy.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+
+from unittest import mock
+
+from lib.assessment.rubric_tester import (
+    main,
+)
+
+accuracy = pytest.mark.skipif("not config.getoption('accuracy')")
+
+@accuracy
+@pytest.mark.accuracy_setup
+class TestAccuracy:
+    def test_accuracy(self):
+        assert "OPENAI_API_KEY" in os.environ
+        with mock.patch('sys.argv', ['rubric_tester.py', '-a']):
+            ret = main()
+        assert ret == True
